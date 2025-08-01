523cb3816f5534a81c96235bba85c451e1357ffa
shisuke-glitch <i0.0nasuke@gmail.com>
2025-08-01 23:47:17 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,24 @@
+{\rtf1\ansi\ansicpg932\cocoartf2820
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;\f1\fnil\fcharset128 HiraginoSans-W3;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 // 
+\f1 \'8e\'a9\'95\'aa\'82\'cc
+\f0  Firebase 
+\f1 \'90\'dd\'92\'e8\'8f\'ee\'95\'f1\'82\'f0\'82\'b1\'82\'b1\'82\'c9\'93\'5c\'82\'c1\'82\'c4\'82\'ad\'82\'be\'82\'b3\'82\'a2
+\f0 \
+export const firebaseConfig = \{\
+  apiKey: "AIzaSyCwRAgSfOOPpOrEH7wJdCmLHtOgJOb2ZKg",\
+  authDomain: "quiz-app-ab0b2.firebaseapp.com",\
+  databaseURL: "https://quiz-app-ab0b2-default-rtdb.firebaseio.com",\
+  projectId: "quiz-app-ab0b2",\
+  storageBucket: "quiz-app-ab0b2.firebasestorage.app",\
+  messagingSenderId: "825831547139",\
+  appId: "1:825831547139:web:e49f693e37afa444b18936",\
+  measurementId: "G-RYX5Z4YHDC"\
+\};\
+\
+firebase.initializeApp(firebaseConfig);}
